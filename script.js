c0752eb583e7247ee9112e4fbbecf4112746517a
midedev <ibrahimayomide330@gmail.com>
2022-09-05 05:28:31 +0100

    implemented logic to get data from api through user search

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+// Declare variables
+const input = document.querySelector(".input");
+const search = document.querySelector(".search");
+
+// GET data from OMBD API
+async function getMovieData() {
+  const searchInput = input.value;
+  const response = await fetch(
+    `http://www.omdbapi.com/?apikey=e639720b&s=${searchInput}`
+  );
+  const movies = await response.json();
+  console.log(movies);
+}
+
+search.addEventListener("click", getMovieData);
