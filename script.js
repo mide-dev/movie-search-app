731e05521c27c3d243389bd617358a6a15c03530
midedev <ibrahimayomide330@gmail.com>
2022-09-20 05:10:48 +0100

    removed search button, added functionality for auto-search. implemented a preLoader

--- script.js
+++ script.js
@@ -3,25 +3,32 @@
 // Declare variables
 const input = document.querySelector(".input");
 const search = document.querySelector(".search");
+const searchBar = document.querySelector(".search-bar");
 const displayMovies = document.querySelector(".display-movies");
 const searchError = document.querySelector(".wrong-search");
 const movieDisplayContainer = document.querySelector(".content-wrap");
 const posterImage = document.querySelector(".movie-img");
 const preLoader = document.querySelector(".loader");
 
+// Hide loader
+const hideLoader = () => {
+  preLoader.classList.add("hide-loader");
+};
+
+// show loader
+const showLoader = () => {
+  preLoader.classList.remove("hide-loader");
+};
+
 // display error
-function displayError(
-  msg = `Unable to find what you’re looking for.
-  Please try another search.`
-) {
+function displayError(msg) {
+  hideLoader();
   searchError.classList.remove("hidden");
   movieDisplayContainer.classList.remove("bg-home");
   searchError.textContent = msg;
 }
 
-function displayLoader() {
-  preLoader.style.display = "none";
-}
+hideLoader();
 
 // html & css to display movie data
 const html = function (mov) {
@@ -31,15 +38,15 @@ const html = function (mov) {
         <div class="movie-content">
             <div class="title-ratings-wrap">
             <h2 class="movie-title">${mov.Title}</h2>
-            <i class="fa-solid fa-star fa-2xs icon"></i>
+            <i class="fa-solid fa-star fa-xs icon"></i>
             <span class="movie-rating">${mov.imdbRating}</span>
             </div>
             <div class="min-desc-wrap">
             <p>${mov.Runtime}</p>
             <p>${mov.Genre}</p>
-            <p class="movie-watchlist">
-                <i class="fa-solid fa-circle-plus watchlist-icon"></i>Watchlist
-            </p>
+            <button class="watchlist-btn">
+                <i class="fa-solid fa-circle-plus fa-lg watchlist-icon"></i>Watchlist
+            </button>
             </div>
             <p class="movie-body">${mov.Plot}</p>
         </div>
@@ -50,28 +57,22 @@ const html = function (mov) {
 // GET search data from OMBD API
 async function getSearchResult() {
   try {
+    showLoader();
     const searchInput = input.value;
     const searchResult = await fetch(
       `http://www.omdbapi.com/?apikey=e639720b&s=${searchInput}`
     );
     // throw err if ntwrk fails
     if (!searchResult.ok) {
-      throw new Error(
-        displayError("Netwrok Error. Please check your internet connection")
-      );
+      throw new Error("Netwrok Error. Please check your internet connection");
     }
 
     const movies = await searchResult.json();
-    // throw err if can't find movie title
-    if (movies.Response === "False") {
-      throw new Error(displayError());
-    }
-    //
+
     return movies.Search;
   } catch (err) {
-    displayError();
-    // rethrow error to catch outside func
-    throw err;
+    // error been handled in displayMovieData()
+    return;
   }
 }
 
@@ -95,43 +96,87 @@ async function getMovieData() {
     const retrieveMovies = await Promise.all(ids);
     return retrieveMovies;
   } catch (err) {
-    displayError();
+    // error been handled in displayMovieData()
+    return;
   }
 }
 
 // display data gotten from "getSearchResult()" to UI
-async function displayMovieData() {
+async function displayMovieData(e) {
+  e.preventDefault();
+
   try {
     // import getMovieData func()
     let movieDataArray = await getMovieData();
 
-    // if movie not found, clear previous search and display error.
+    // if movie not found, clear previous search and handle error.
     if (movieDataArray === undefined) {
       while (displayMovies.firstChild) {
         displayMovies.removeChild(displayMovies.lastChild);
       }
+
+      if (input.value.length === 0) {
+        hideLoader();
+        searchError.classList.add("hidden");
+        movieDisplayContainer.classList.add("bg-home");
+        return;
+      }
+
+      if (input.value.length > 0 && input.value.length < 3) {
+        throw Error("Please Input minimum of 3 characters");
+      }
+
+      /* If cant find movie (wait for 2sec to delay firing...
+      incase user is not done typing) */
+      if (input.value.length > 3) {
+        setTimeout(() => {
+          displayError(`Unable to find what you’re looking for.
+          Please try another search.`);
+        }, 2000);
+      }
+
       return;
     }
 
-    // remove error msg if movie found
+    // if movie found
+    // remove the default bg-icon
+    movieDisplayContainer.classList.remove("bg-home");
+
+    // remove error msg
     searchError.classList.add("hidden");
 
-    // clear previous search and display new data
+    // clear previous search
     while (movieDataArray && displayMovies.firstChild) {
       displayMovies.removeChild(displayMovies.lastChild);
     }
 
+    // hide preLoader
+    hideLoader();
     console.log(movieDataArray);
-    // remove the default bg-icon
-    movieDisplayContainer.classList.remove("bg-home");
 
-    // display the data
+    // for (let i of movieDataArray) {
+    //   if (watchlist-btn.addEventListener('click')
+    // }
+    // display movies
     movieDataArray.forEach((mov) => {
       displayMovies.insertAdjacentHTML("beforeend", html(mov));
     });
   } catch (err) {
-    displayError();
+    displayError(err.message);
+    return;
   }
 }
 
-search.addEventListener("click", displayMovieData);
+searchBar.addEventListener("submit", displayMovieData);
+
+// search for title as user types
+let timeout;
+
+input.addEventListener("keyup", function (e) {
+  // clear timeout if already set
+  // prevent previous search from executing
+  clearTimeout(timeout);
+
+  // Make a new timeout set to go off in 200ms (.2 second)
+  timeout = setTimeout(displayMovieData.bind(null, e), 200);
+});
